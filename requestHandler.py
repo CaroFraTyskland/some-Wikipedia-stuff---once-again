d7042d8e38789a427da1e6c8ef12df7de865b995
CaroFraTyskland <bruckmeier@fim.uni-passau.de>
2022-09-22 18:17:48 +0200

    added get_html with urllib.request

--- requestHandler.py
+++ requestHandler.py
@@ -1,7 +1,26 @@
 import requests
+import urllib.request
+
 
 #retrieves data from a given url and returns it
 def get_json_code(URL):
     response = requests.get(URL, timeout=0.5)
 
     return response.json()
+
+#retrieves html from a given url and returns it
+def get_html_code(URL):
+    fp = urllib.request.urlopen(URL)
+    html = fp.read().decode("utf8")
+    fp.close()
+
+    return html
+
+#retrieves html from a given url and returns it
+def get_html_code_without_head(URL):
+    html = get_html_code(URL)
+
+    index = html.index("</head>") + 7
+    html = html[index:]
+
+    return html
