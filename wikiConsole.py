143072298f240c9b3535d19c89bc91e02255a701
CaroFraTyskland <bruckmeier@fim.uni-passau.de>
2022-09-22 11:37:33 +0200

    added possibility to list subcategories

--- wikiConsole.py
+++ wikiConsole.py
@@ -1,10 +1,11 @@
-import articlesFromCategory
+import categoryMembers
 import copyVioCheck
 
 def print_help():
-    help = "cat:\tprint articles from category\n"
+    help = "cat:\tprints articles from a category and its subcategories\n"
     help = help + "cv:\tchecks articles for possible copyright violations (via copyvios.toolforge.org) -> can take forever\n"
-    help = help + "help:\toverview over commands"
+    help = help + "help:\toverview over commands\n"
+    help = help + "sc:\tprints subcategories from a category and its subcategories"
 
     print (help)
 
@@ -14,7 +15,7 @@ while (True):
 
     if command == "cat":
         cat = input("Name der Kategorie: ")
-        articlesFromCategory.print_articles_cat(cat)
+        categoryMembers.print_articles_cat(cat)
 
     elif command == "copyvios" or command == "cv":
         article = input("zu prüfender Artikel: ")
@@ -22,3 +23,7 @@ while (True):
 
     elif command == "help" or command == "h":
         print_help()
+
+    elif command == "subcats" or command == "sc":
+        cat = input("Name der Kategorie: ")
+        categoryMembers.print_subcats_cat(cat)
