08317e8112502de1b2e86e4985cfb8cfb8b7fce9
CaroFraTyskland <bruckmeier@fim.uni-passau.de>
2022-09-29 13:03:50 +0200

    get coordinates from the api

---
+++ coordinates.py
@@ -0,0 +1,13 @@
+import math
+from decimal import *
+
+def convert_coordinates(coordinates):
+    coord = Decimal(coordinates)
+    hours = math.floor(coord)
+
+    min_and_sec = (Decimal(coord) - Decimal(hours)) * 60
+    minutes = math.floor(min_and_sec)
+
+    seconds = math.floor((min_and_sec - minutes) * 60)
+    
+    return str(hours) + "/" + str(minutes) + "/" + str(seconds)
--- kartverketAPI.py
+++ kartverketAPI.py
@@ -1,8 +1,15 @@
 import requestHandler
 import fylke
+import coordinates
 
 def get_code_muni_basic(muni_nr):
     return requestHandler.get_json_code("https://ws.geonorge.no/kommuneinfo/v1/kommuner/" + str(muni_nr))
+
+def get_code_stedsnavn_kommune(name, muni_nr):
+    return requestHandler.get_json_code("https://ws.geonorge.no/stedsnavn/v1/sted?sok=" + name + "&fuzzy=false&knr=" + str(muni_nr) + "&navneobjekttype=kommune&utkoordsys=4258&treffPerSide=10&side=1")
+
+def get_code_stedsnavn_tettsted(name, muni_nr):
+    return requestHandler.get_json_code("https://ws.geonorge.no/stedsnavn/v1/sted?sok=" + name + "&fuzzy=false&knr=" + str(muni_nr) + "&navneobjekttype=by&navneobjekttype=tettsted&navneobjekttype=bygdelagBygd&utkoordsys=4258&treffPerSide=10&side=1")
 
 def get_municipality_name(code_muni_basic):
     return code_muni_basic["kommunenavn"]
@@ -27,3 +34,9 @@ def get_neighbour_munis(muni_nr):
         neighbours.append(muni["kommunenummer"])
 
     return neighbours
+
+def get_coord_long(code_stedsnavn):
+    return coordinates.convert_coordinates(code_stedsnavn["navn"][0]["representasjonspunkt"]["øst"])
+
+def get_coord_lat(code_stedsnavn):
+    return coordinates.convert_coordinates(code_stedsnavn["navn"][0]["representasjonspunkt"]["nord"])
